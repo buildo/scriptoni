ab620b34aa59884fcfbb2c7cbef05d7bb85d88bc
Andrea Ascari <andrea.ascari@buildo.io>
2016-11-04 17:38:13 +0100

    Added utility function to bin script and define lint style files task.

--- bin/scriptoni.js
+++ bin/scriptoni.js
@@ -4,15 +4,22 @@ var spawn = require('cross-spawn');
 var script = process.argv[2];
 var args = process.argv.slice(3);
 
+function spawnScript(script) {
+  const cmd = [require.resolve('../lib/scripts/' + script)].concat(args);
+  return spawn.sync('node', cmd, { stdio: 'inherit' });
+}
+
+function exit(result) {
+  process.exit(result.status);
+}
+
 switch (script) {
   case 'metarpheus':
   case 'metarpheus-diff':
-    var result = spawn.sync(
-      'node',
-      [require.resolve('../lib/scripts/' + script)].concat(args),
-      {stdio: 'inherit'}
-    );
-    process.exit(result.status);
+    exit(spawnScript(script));
+    break;
+  case 'lint-style':
+    exit(spawnScript('stylelint'));
     break;
   default:
     console.log('Unknown script "' + script + '".');
