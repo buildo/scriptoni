04e28f02c79192178dc36561c430a738da050ab5
Vincenzo Guerrisi <vincenzo@buildo.io>
2017-02-08 15:28:00 +0100

    replace linting configs with js file to be included in lib for release

--- src/scripts/stylelint/index.js
+++ src/scripts/stylelint/index.js
@@ -2,7 +2,7 @@ import fs from 'fs';
 import path from 'path';
 import { lint } from 'stylelint';
 import { red } from 'chalk';
-import { resolveInSrc } from '../../util';
+import baseConfig from './stylelintrc';
 
 const cwd = process.cwd();
 
@@ -13,7 +13,6 @@ function readConfigInDir(dir) {
 
 function getConfig() {
   const userStylelintrc = readConfigInDir(cwd);
-  const baseConfig = require(resolveInSrc('stylelint/stylelintrc.json'));
 
   return {
     config: {
