0a195340604431d86a9157b7e669c25bf16259f6
Vincenzo Guerrisi <vincenzo@buildo.io>
2017-03-31 12:01:03 +0200

    eslint handles additional args

--- src/scripts/eslint/index.js
+++ src/scripts/eslint/index.js
@@ -1,13 +1,37 @@
-import { CLIEngine } from 'eslint';
+import path from 'path';
+import { execSync } from 'child_process';
+import minimist from 'minimist';
+import map from 'lodash/map';
+import t from 'tcomb';
 
-const baseConfig = require('./eslintrc.json');
+const cwd = process.cwd();
 
-const cli = new CLIEngine({ baseConfig });
-const formatter = cli.getFormatter('stylish');
+const userArgs = minimist(process.argv.slice(2));
 
-const report = cli.executeOnFiles(['src']);
-console.log(formatter(report.results));     // eslint-disable-line no-console
+const args = {
+  cache: true,
+  ...userArgs,
+  _: userArgs._.length > 0 ? userArgs._ : ['src']
+};
 
-if (report.errorCount > 0) {
+const eslintCmd = [
+  path.resolve(cwd, 'node_modules', 'eslint', 'bin', 'eslint.js'),
+  ...map(args, (v, k) => {
+    if (k !== '_') {
+      if (t.Bool.is(v)) {
+        return `--${k}`;
+      } else {
+        return `--${k} ${v}`;
+      }
+    } else {
+      return v.join(' ');
+    }
+  })
+];
+
+try {
+  execSync(eslintCmd.join(' '), { stdio: 'inherit' });
+} catch (err) {
   process.exit(1);
 }
+
