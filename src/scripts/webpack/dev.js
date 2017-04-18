58978ec3468e77fb40ceaa40982a7ea7891dfe51
Andrea Ascari <andrea.ascari@buildo.io>
2017-04-18 17:31:33 +0200

    Enabled hmr for webpack-dev task.

--- src/scripts/webpack/dev.js
+++ src/scripts/webpack/dev.js
@@ -3,13 +3,16 @@ import webpackServer from 'webpack-dev-server';
 import webpackConfig from './webpack.config.js';
 import compiler from './compiler';
 import getConfig from './config';
+import getPaths from './paths';
 import { statsOutputConfiguration } from './util';
 
 const args = minimist(process.argv.slice(2));
+const paths = getPaths(args);
 
 const server = new webpackServer(compiler(webpackConfig), {
-  stats: statsOutputConfiguration,
-  hot: true
+  contentBase: paths.BUILD,
+  hot: true,
+  stats: statsOutputConfiguration
 });
 
 server.listen(getConfig(args).port);
