4048274f34d3a429bb3a97cd01f1d0729fc9e52e
Giovanni Gonzaga <giovanni@buildo.io>
2018-05-18 14:10:59 +0200

    fix runCcommands

--- test/globalSetup.js
+++ test/globalSetup.js
@@ -5,14 +5,18 @@ const templateDir = resolve(__dirname, 'template-app');
 
 function runCommands(commands) {
   return new Promise((resolve, reject) => {
-    const shell = spawn('bash', ['-c', commands.join(' && ')], { stdio: 'inherit' });
-    shell.on('exit', code => {
-      if (code === 1) {
-        reject();
-      } else {
-        resolve();
-      }
-    });
+    try {
+      const shell = spawn('bash', ['-c', commands.join(' && ')], { stdio: 'inherit' });
+      shell.on('close', code => {
+        if (code === 1) {
+          reject(1);
+        } else {
+          resolve(0);
+        }
+      });
+    } catch (e) {
+      reject(e);
+    }
   });
 }
 
--- test/utils.ts
+++ test/utils.ts
@@ -5,15 +5,17 @@ export const templateDir = resolve(__dirname, 'template-app');
 
 export function runCommands(commands: string[]) {
   return new Promise((resolve, reject) => {
-    const shell = spawn('bash', [], { stdio: 'inherit' });
-    console.log(shell);
-    shell.stdin.write(commands.join(' && '));
-    shell.on('exit', code => {
-      if (code === 1) {
-        reject();
-      } else {
-        resolve();
-      }
-    });
+    try {
+      const shell = spawn('bash', ['-c', commands.join(' && ')], { stdio: 'inherit' });
+      shell.on('close', code => {
+        if (code === 1) {
+          reject(1);
+        } else {
+          resolve(0);
+        }
+      });
+    } catch (e) {
+      reject(e);
+    }
   });
 }
