ac10679c3c30d99d3ca7b96ac47f93367289561f
Giovanni Gonzaga <giovanni@buildo.io>
2018-05-18 12:02:40 +0200

    show stdout of runCommands (WIP)

--- test/globalSetup.js
+++ test/globalSetup.js
@@ -1,15 +1,18 @@
 const resolve = require('path').resolve;
-const _exec = require('child_process').exec;
-const promisify = require('util').promisify;
-
-const exec = promisify(_exec);
+const spawn = require('child_process').spawn;
 
 const templateDir = resolve(__dirname, 'template-app');
 
 function runCommands(commands) {
-  return exec(commands.join(' && ')).catch(err => {
-    console.error(err);
-    throw err;
+  return new Promise((resolve, reject) => {
+    const shell = spawn('bash', ['-c', commands.join(' && ')], { stdio: 'inherit' });
+    shell.on('exit', code => {
+      if (code === 1) {
+        reject();
+      } else {
+        resolve();
+      }
+    });
   });
 }
 
@@ -21,11 +24,9 @@ module.exports = () => {
   console.log();
   return runCommands([
     `cd ${templateDir}`,
-    'yarn --no-lockfile',
-    'rm -rf node_modules/scriptoni',
     'cd ../../',
     'yarn build',
     `cd ${templateDir}`,
-    'yarn add --no-lockfile ../../'
+    'yarn --no-lockfile'
   ]);
 };
--- test/utils.ts
+++ test/utils.ts
@@ -1,14 +1,19 @@
 import { resolve } from 'path';
-import { exec as _exec } from 'child_process';
-import { promisify } from 'util';
-
-const exec = promisify(_exec);
+import { spawn } from 'child_process';
 
 export const templateDir = resolve(__dirname, 'template-app');
 
 export function runCommands(commands: string[]) {
-  return exec(commands.join(' && ')).catch(err => {
-    console.error(err);
-    throw err;
-  })
+  return new Promise((resolve, reject) => {
+    const shell = spawn('bash', [], { stdio: 'inherit' });
+    console.log(shell);
+    shell.stdin.write(commands.join(' && '));
+    shell.on('exit', code => {
+      if (code === 1) {
+        reject();
+      } else {
+        resolve();
+      }
+    });
+  });
 }
