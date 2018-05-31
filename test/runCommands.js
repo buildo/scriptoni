9977646cad8121911f72636e936627c59d2cbaed
Francesco Cioria <firefelix@gmail.com>
2018-05-31 16:56:29 +0200

    spawn "cmd" in windows

--- test/runCommands.js
+++ test/runCommands.js
@@ -1,9 +1,15 @@
 const spawn = require('child_process').spawn;
 
+const isWin = process.platform === 'win32';
+
 module.exports = function runCommands(commands) {
   return new Promise((resolve, reject) => {
     try {
-      const shell = spawn('bash', ['-c', commands.join(' && ')], { stdio: 'inherit' });
+      const shell = spawn(
+        isWin ? 'cmd' : 'bash',
+        (isWin ? ['/c'] : ['-c']).concat([commands.join(' && ')]),
+        { stdio: 'inherit' }
+      );
       shell.on('close', code => {
         if (code === 1) {
           reject(1);
