5e2e6af6b1241be7744758ddebb62a81de7afaf9
Francesco Cioria <firefelix@gmail.com>
2018-11-16 12:56:20 +0100

    fix tests

--- test/globalSetup.js
+++ test/globalSetup.js
@@ -1,21 +1,15 @@
-const resolve = require('path').resolve;
-const runCommands = require('./runCommands');
+const resolve = require("path").resolve;
+const runCommands = require("./runCommands");
 
-const templateDir = resolve(__dirname, 'template-app');
-const scriptoniDir = resolve(__dirname, '..');
+const scriptoniDir = resolve(__dirname, "..");
 
 module.exports = () => {
   console.log();
   console.log();
-  console.log('🕒 Preparing to run the tests! This will take a few minutes...');
-  console.log('🕒 Please be patient (or smart and improve `scriptoni/test/globalSetup.js`)');
+  console.log("🕒 Preparing to run the tests! This will take a few minutes...");
+  console.log(
+    "🕒 Please be patient (or smart and improve `scriptoni/test/globalSetup.js`)"
+  );
   console.log();
-  return runCommands([
-    `cd ${scriptoniDir}`,
-    'yarn build',
-    `cd ${templateDir}`,
-    'rm -rf node_modules',
-    'yarn',
-    'rm -rf node_modules/@buildo/bento/node_modules/scriptoni'
-  ]);
+  return runCommands([`cd ${scriptoniDir}`, "yarn build"]);
 };
--- test/metarpheus.test.ts
+++ test/metarpheus.test.ts
@@ -1,18 +1,18 @@
-import { runCommands, templateDir } from './utils';
-import * as fs from 'fs';
-import * as rimraf from 'rimraf';
+import { runCommands, templateDir } from "./utils";
+import * as fs from "fs";
+import * as rimraf from "rimraf";
 
 jest.setTimeout(10 * 60 * 1000);
 
-describe('metarpheus', () => {
-  describe('metarpheusConfig option', () => {
-    it('should read the correct config file', () => {
+describe("metarpheus", () => {
+  describe("metarpheusConfig option", () => {
+    it("should read the correct config file", () => {
       const destinationDir = `${templateDir}/src/metarpheus/`;
       return runCommands([
         `cd ${templateDir}`,
         // this will fail if the `metarpheusConfig` param is not considered,
         // since the default config file that's used otherwise imports scala from an inexisting folder
-        './node_modules/.bin/scriptoni metarpheus --ts --metarpheusConfig ./metarpheus.config.test.js'
+        "yarn metarpheus"
       ]).then(() => {
         // checking that the directory has been created and then
         // removing it so that it isn't included in the build afterwards
