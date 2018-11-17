9797aa51c05542d493253d51c443622d09f82c3d
Francesco Cioria <firefelix@gmail.com>
2018-11-17 13:50:04 +0100

    add api/model prelude to test metarpheus config

--- test/template-app/metarpheus.config.test.js
+++ test/template-app/metarpheus.config.test.js
@@ -1,5 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  apiPaths: [path.resolve(__dirname, './fake-api-src')]
+  apiPaths: [path.resolve(__dirname, './fake-api-src')],
+  apiPrelude: 'API_PRELUDE',
+  modelPrelude: 'MODEL_PRELUDE'
 };
