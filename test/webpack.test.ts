0e475341c0355d6c6e36617eddf5f4507c0f18f1
Francesco Cioria <firefelix@gmail.com>
2018-12-30 21:21:25 +0100

    add new test to verify check imported files in HTML

--- test/webpack.test.ts
+++ test/webpack.test.ts
@@ -1,7 +1,7 @@
 import * as rimraf from 'rimraf';
 import { resolve } from 'path';
 import { promisify } from 'util';
-import { readdir as _readdir, statSync, mkdirSync } from 'fs';
+import { readdir as _readdir, statSync, mkdirSync, readFileSync } from 'fs';
 import sortBy = require('lodash/sortBy');
 import { runCommands, templateDir } from './utils';
 
@@ -22,6 +22,27 @@ describe('webpack', () => {
     it('built files should stay the same', async () => {
       const fileNames = await readdir(resolve(templateDir, 'build'));
       expect(fileNames.map(stripHash)).toMatchSnapshot();
+    });
+
+    it('index.html should import CSS and JS files', async () => {
+      const htmlFile = readFileSync(resolve(templateDir, 'build', 'index.html'), {
+        encoding: 'utf8'
+      });
+
+      expect(
+        /<link rel="stylesheet" href="\/vendors~main\.style\..+\.min\.css" media="all">/.test(
+          htmlFile
+        )
+      ).toBe(true);
+      expect(
+        /<link rel="stylesheet" href="\/main\.style\..+\.min\.css" media="all">/.test(htmlFile)
+      ).toBe(true);
+      expect(
+        /<script type="text\/javascript" src="\/vendors~main\.bundle\..+\.js">/.test(htmlFile)
+      ).toBe(true);
+      expect(/<script type="text\/javascript" src="\/main\.bundle\..+\.js">/.test(htmlFile)).toBe(
+        true
+      );
     });
 
     it('built files should not change size', async () => {
