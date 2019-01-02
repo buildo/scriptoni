516eacb4f5c39c76776dc6a4e4e0e8e3d92ee994
Francesco Cioria <firefelix@gmail.com>
2019-01-02 17:00:24 +0100

    prettify scss files too

--- src/scripts/prettier/.prettierrc.js
+++ src/scripts/prettier/.prettierrc.js
@@ -1,5 +1,4 @@
 module.exports = {
   printWidth: 100,
-  parser: 'typescript',
   singleQuote: true
 };
--- src/scripts/prettier/getDefaultArgs.ts
+++ src/scripts/prettier/getDefaultArgs.ts
@@ -1,7 +1,7 @@
 import * as path from 'path';
 
 const configPath = path.join(__dirname, '.prettierrc.js');
-const files = '{**/*,*}.{js,jsx,ts,tsx}';
+const files = '**/*.{js,jsx,ts,tsx,scss}';
 
 export default function getCommandsAndDefaultArgs(
   writeOrListDifferent: 'write' | 'list-different'
