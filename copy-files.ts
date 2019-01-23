01702dc291a8024452abad30404e00b399e45003
Francesco Cioria <firefelix@gmail.com>
2019-01-23 15:43:34 +0100

    use prettier default config

--- copy-files.ts
+++ copy-files.ts
@@ -1,10 +1,13 @@
-import * as fs from 'fs';
-import * as path from 'path';
+import * as fs from "fs";
+import * as path from "path";
 
 const copyToLib = (file: string) => {
-  fs.writeFileSync(path.resolve('lib', file), fs.readFileSync(path.resolve('src', file)));
+  fs.writeFileSync(
+    path.resolve("lib", file),
+    fs.readFileSync(path.resolve("src", file))
+  );
 };
 
-const files = ['scripts/webpack/tsconfig.json', 'scripts/prettier/.prettierrc.js'];
+const files = ["scripts/webpack/tsconfig.json"];
 
 files.map(copyToLib);
--- src/scripts/prettier/getDefaultArgs.ts
+++ src/scripts/prettier/getDefaultArgs.ts
@@ -1,13 +1,9 @@
-import * as path from 'path';
-
-const configPath = path.join(__dirname, '.prettierrc.js');
-const files = '**/*.{js,jsx,ts,tsx,scss}';
+const files = "**/*.{js,jsx,ts,tsx,scss}";
 
 export default function getCommandsAndDefaultArgs(
-  writeOrListDifferent: 'write' | 'list-different'
+  writeOrListDifferent: "write" | "list-different"
 ) {
   return {
-    config: configPath,
     [writeOrListDifferent]: files
   };
 }
